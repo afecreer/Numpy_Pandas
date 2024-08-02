c580953bd48cca3ff6f57452b9ba0ffe3ab8beb9
afecreer <your-email@example.com>
2024-08-01 20:30:53 -0500

    trabjando con pandas y python con listas y arreglos

---
+++ elarray.py
@@ -0,0 +1,30 @@
+import numpy as np
+lista = [1,2,3,4,5,6,7,8,9]
+print(lista)
+np.array(lista)
+arr=np.array(lista)
+type(arr)
+matriz=[[1,2,3],[4,5,6],[7,8,9]]
+matriz=np.array(matriz)
+matriz
+arr
+arr[1]
+arr[0]+arr[5]
+matriz[0]
+matriz[1]
+matriz[2]
+matriz[3]
+matriz
+matriz[0,2]
+arr[:6]
+arr[2:]
+arr[:] #asi tambien puedo decirle que quiero todo
+arr[::3]
+arr[-1]
+arr[:-3]
+arr[-3:]
+matriz
+matriz[1:]#recuerdo que el primer valor hacia referencia a la linea 1
+matriz[1:,0:2]
+
+
