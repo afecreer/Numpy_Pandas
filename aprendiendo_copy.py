7a64eda32bb1f865395c6f30e97f6cf5fded3c22
afecreer <your-email@example.com>
2024-09-07 20:13:03 -0500

    Abordamos usado el comando copy

--- aprendiendo_copy.py
+++ aprendiendo_copy.py
@@ -5,10 +5,13 @@ trozo_de_arr = arr[0:6]
 print(arr)
 #A todo el arreglo le quieto qutar todos los valores y dejarlos
 trozo_de_arr[:]=0
-#Si ahora imprimimos el array completo, veras que todos los valores han sido
+print(trozo_de_arr)
+#Si ahora imprimimos el array completo, veras que todos los valores han sido ca,boadps
 print(arr)
 #para solucioar esto utilizaremos el comando copy
 arr_copy = arr.copy()
 arr_copy[:] = 100
-#no obstante la lista original es respetada
+print(arr_copy)
+#no obstante la lista original es respetada !
 print(arr)
+
